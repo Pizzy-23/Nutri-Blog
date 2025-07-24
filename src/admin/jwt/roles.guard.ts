5a8f883ea2d4b782dd5ee64efbe1dcd60fdf0a7c
Pizzy-23 <parzival1.2323@gmail.com>
2025-07-24 13:22:01 -0300

    fix roles guard, e module

--- src/admin/jwt/roles.guard.ts
+++ src/admin/jwt/roles.guard.ts
@@ -1,13 +1,23 @@
+// src/auth/guards/roles.guard.ts
+
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { InjectRepository } from '@nestjs/typeorm'; 
+import { Repository } from 'typeorm';
 import { UserRole } from 'src/user/enums/user-role.enum';
 import { ROLES_KEY } from '../decorators/roles.decorator';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
 
-  canActivate(context: ExecutionContext): boolean {
+    @InjectRepository(User)
+    private adminRepository: Repository<User>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
       ROLES_KEY,
       [context.getHandler(), context.getClass()],
@@ -17,8 +27,19 @@ export class RolesGuard implements CanActivate {
       return true;
     }
 
-    const { user } = context.switchToHttp().getRequest();
+    const { user: userFromJwt } = context.switchToHttp().getRequest();
+    if (!userFromJwt || !userFromJwt.id) {
+      return false;
+    }
 
-    return requiredRoles.some((role) => user.role?.includes(role));
+    const adminFromDb = await this.adminRepository.findOneBy({
+      id: userFromJwt.id,
+    });
+
+    if (!adminFromDb || !adminFromDb.role) {
+      return false;
+    }
+
+    return requiredRoles.some((role) => adminFromDb.role === role);
   }
 }
--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -3,9 +3,10 @@ import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Blog } from './entities/blog.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Blog])],
+  imports: [TypeOrmModule.forFeature([User, Blog])],
   controllers: [BlogController],
   providers: [BlogService]
 })
